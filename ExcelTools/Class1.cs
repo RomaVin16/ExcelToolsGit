b5abe965ec92fb3262893dd3b64f41e70469bc2a
RomaVin16 <159806983+RomaVin16@users.noreply.github.com>
2024-08-15 15:29:23 +0500

    Добавьте файлы проекта.

---
+++ ExcelTools/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ExcelTools
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ExcelToolsApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExcelToolsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
